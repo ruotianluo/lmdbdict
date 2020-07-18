53aaed901946626d42750bc96c5920c82e91fbe6
Ruotian Luo <rluo@ttic.edu>
2020-07-18 11:03:03 -0500

    add version to the module.(learn from d2)

--- lmdbdict/__init__.py
+++ lmdbdict/__init__.py
@@ -1,1 +1,3 @@
 from .lmdbdict import lmdbdict, LMDBDict
+
+__version__ = "0.2"
--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
 import setuptools
+from os import path
+
+# from detectron2
+def get_version():
+    init_py_path = path.join(path.abspath(path.dirname(__file__)), "lmdbdict", "__init__.py")
+    init_py = open(init_py_path, "r").readlines()
+    version_line = [l.strip() for l in init_py if l.startswith("__version__")][0]
+    version = version_line.split("=")[-1].strip().strip("'\"")
+    return version
 
 setuptools.setup(
     name="lmdbdict",
-    version="0.1",
+    version=get_version(),
     author="Ruotian Luo",
     author_email="rluo@ttic.edu",
     packages=setuptools.find_packages(),
