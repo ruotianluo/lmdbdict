4ee6b07a62df011f3af3d53fbdc0c8755b6f8f13
Ruotian Luo <rluo@ttic.edu>
2020-07-21 10:34:29 -0700

    Enhance picklablewrapper: save both pickle and cloudpickle dumps so that the receiver and sender

--- lmdbdict/utils.py
+++ lmdbdict/utils.py
@@ -9,13 +9,30 @@ else:
 
 
 def picklable_wrapper(obj):
-    # Wrap the object with PicklableWrapper only if it's not natively picklable
-    # Note: it's not intended to be run a lot of times
+    """
+    Wrap the object with PicklableWrapper only if it's not natively picklable
+    Note: it's not intended to be run a lot of times
+    """
     try:
         pickle.dumps(obj)
         return obj
     except:
         return PicklableWrapper(obj)
+
+
+def loads_either(cloudpickle_out, pickle_out):
+    r"""
+    If cloudpickle dumps is available then load with cloudpickle first,
+    because cloudpickle is safer to use for callabels then pickle.
+    If not availble, then resort to pickle.
+    """
+    if CLOUDPICKLE_AVAILABLE and cloudpickle_out is not None:
+        return cloudpickle.loads(cloudpickle_out)
+    elif pickle_out is not None:
+        return pickle.loads(pickle_out)
+    else:
+        raise ValueError('Not able to load the pickled object')
+
 
 
 class PicklableWrapper(object):
@@ -25,14 +42,25 @@ class PicklableWrapper(object):
     It's best to use it only on closures (which are usually not picklable).
     This is a simplified version of
     https://github.com/joblib/joblib/blob/master/joblib/externals/loky/cloudpickle_wrapper.py
+    RT change: we save both cloudpickle and pickle dumps.
+    This is for the case that sender and receiver may not have the same enviroment.
+    cloudpickle is preferred because it's better for callables.
     """
 
     def __init__(self, obj):
         self._obj = obj
 
     def __reduce__(self):
-        s = cloudpickle.dumps(self._obj)
-        return cloudpickle.loads, (s,)
+        if CLOUDPICKLE_AVAILABLE:
+            s = cloudpickle.dumps(self._obj)
+        else:
+            s = None
+        try:
+            s_ = pickle.dumps(self._obj)
+        except:
+            s_ = None
+        assert not (s is None and s_ is None), 'Fail to dump'
+        return loads_either, (s, s_)
 
     def __call__(self, *args, **kwargs):
         return self._obj(*args, **kwargs)
---
+++ tests/test_picklable_wrapper.py
@@ -0,0 +1,47 @@
+import pytest
+from lmdbdict.utils import PicklableWrapper, picklable_wrapper
+import pickle
+try:
+    import cloudpickle
+except:
+    CLOUDPICKLE_AVAILABLE = False
+else:
+    CLOUDPICKLE_AVAILABLE = True
+from unittest import mock
+
+
+@pytest.mark.skipif(not CLOUDPICKLE_AVAILABLE, reason="PickableWrapper requires cloudpickle")
+def test_picklablewrapper():
+    fn_bytes = pickle.dumps(PicklableWrapper(lambda x: x))
+    fn = pickle.loads(fn_bytes)
+
+    assert fn(1) == 1
+
+
+def test_picklable_wrapper():
+    # If object is pickable, it should remain the same after called picklable_wrapper
+    tmp = 1
+    assert pickle.dumps(tmp) == pickle.dumps(picklable_wrapper(tmp))
+
+def _temporary_func():
+    return 1
+
+def test_no_cloudpickle():
+    # If object is pickable, it should remain the same after called picklable_wrapper
+
+    # with cloudpickle
+    tmp = lambda x: x
+    tmp = pickle.dumps(PicklableWrapper(tmp))
+    with mock.patch('lmdbdict.utils.CLOUDPICKLE_AVAILABLE', False):
+        with mock.patch('lmdbdict.utils.cloudpickle', pickle):
+            # without cloudpickle
+            with pytest.raises(ValueError):
+                pickle.loads(tmp)
+
+    
+    # with cloudpickle
+    tmp = pickle.dumps(PicklableWrapper(_temporary_func))
+    with mock.patch('lmdbdict.utils.CLOUDPICKLE_AVAILABLE', False):
+        with mock.patch('lmdbdict.utils.cloudpickle', pickle):
+            # without cloudpickle
+            pickle.loads(tmp)
